284af8ccd88bdca6fbe503f581a1814487c8f9d3
Kate Ramizovskaya <kr978407@gmail.com>
2024-10-03 11:19:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-number = int(input("Введите целое число : "))
+number = int(input("Введите целое число : "))# ввод числа с клавиатуры
 
-absolute_value = number if number >= 0 else -number
+absolute_value = number if number >= 0 else -number # тернарное выражение
 
-print("Абсолютное значение:", absolute_value)
+print("Абсолютное значение:", absolute_value) # вывод результата
